6d1ab3c5b7caeea878a7615cc6f7cf9e9600bb8c
Andrea Bardi <andbardii@icloud.com>
2025-03-30 13:44:00 +0200

    add little todo

--- src/lib/browserDatabase.ts
+++ src/lib/browserDatabase.ts
@@ -280,6 +280,7 @@ class BrowserDatabaseService {
     const db = await this.getDatabase();
     const numericId = parseInt(id, 10);
     
+    // eslint-disable-next-line no-async-promise-executor
     return new Promise(async (resolve, reject) => {
       const transaction = db.transaction([this.requestsStoreName], 'readwrite');
       const store = transaction.objectStore(this.requestsStoreName);
